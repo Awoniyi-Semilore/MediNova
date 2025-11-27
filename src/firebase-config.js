aa0355ecfd72ac719b66d55a2be42a294e9c063d
Awoniyi-Semilore <semiloreawoniyi@gmail.com>
2025-11-27 13:26:31 +0100

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "'./config/firebase.js";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
