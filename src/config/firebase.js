89f37682b2cd4d89f57b41d422a93b8f1a425d62
Awoniyi-Semilore <semiloreawoniyi@gmail.com>
2025-11-28 08:57:48 +0100

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,29 +1,17 @@
-// lib/firebase.js
-import { initializeApp, getApps } from 'firebase/app';
-import { getAuth, setPersistence, inMemoryPersistence } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyD_kxeWXHdwi3MvhGSQ7AxE-S_BqiZHHfw",
+  authDomain: "medinova-simulations.firebaseapp.com",
+  projectId: "medinova-simulations",
+  storageBucket: "medinova-simulations.firebasestorage.app",
+  messagingSenderId: "473481141664",
+  appId: "1:473481141664:web:fdbcb2642a030e537218c5"
 };
 
-// Initialize Firebase only once
-let app;
-if (!getApps().length) {
-  app = initializeApp(firebaseConfig);
-} else {
-  app = getApps()[0];
-}
-
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-// This makes auth much faster
-setPersistence(auth, inMemoryPersistence);
-
-export { auth, db, app };
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export default app;
